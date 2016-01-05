ebcae4878e0c22dfe8759dc292a173120636bde9
wrights <wrights@teds.com>
2016-01-05 09:44:38 -0500

    Initial commit with webpack, hot-reloading, and linting set up for react project

---
+++ app/component.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+  var element = document.createElement('h1');
+
+  element.innerHTML = 'Hello world, and Good Bye, its been nice knowing you';
+
+  return element;
+};
